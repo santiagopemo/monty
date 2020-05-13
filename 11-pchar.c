53286e4e1a47ba0eb2477dcdd5de0b7998ce1f22
santiagopemo <santiagopemo@gmail.com>
2020-05-13 20:39:13 +0000

    added task 11

--- 11-pchar.c
+++ 11-pchar.c
@@ -14,10 +14,11 @@ void _pchar(stack_t **stack, unsigned int line_number)
 		dprintf(STDERR_FILENO, "L%d: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	if ((*stack)->n < -1 || (*stack)->n > 127)
+	if (isascii((*stack)->n))
 	{
-		dprintf(STDERR_FILENO, "L%d: can't pchar, value out of range\n", line_number);
-		exit(EXIT_FAILURE);
+		printf("%c\n", (*stack)->n);
+		return;
 	}
-	printf("%c\n", (*stack)->n);
+	dprintf(STDERR_FILENO, "L%d: can't pchar, value out of range\n", line_number);
+	exit(EXIT_FAILURE);
 }
